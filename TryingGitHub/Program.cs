e26ae6fd6498bacf513e6a545a4389d56cd4918b
KamilaPolakova <kamila.polakova@mensa.cz>
2024-04-30 17:31:23 +0200

    lol velká změna udělána...

--- TryingGitHub/Program.cs
+++ TryingGitHub/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+//změnila jsem toho stejně jako kolik dělám věcí na ročníkovku
+
+int a = 0;
